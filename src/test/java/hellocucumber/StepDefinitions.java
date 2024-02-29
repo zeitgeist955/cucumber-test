9d8bd8335ba02b8239ea006869735a14eb152426
Nicolas Deman <nicolas.deman@hotmail.fr>
2024-02-29 15:43:16 +0100

    project setup with cucumber
    remove next step code

---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,24 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
+
