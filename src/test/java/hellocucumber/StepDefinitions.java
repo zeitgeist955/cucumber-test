26bc5dac1a1381545df8472dc668accf36517273
Nicolas Deman <nicolas.deman@hotmail.fr>
2024-02-29 15:46:29 +0100

    add is it friday basic test

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -8,6 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinitions {
 
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
     @Given("an example scenario")
     public void anExampleScenario() {
     }
@@ -22,3 +40,8 @@ public class StepDefinitions {
 
 }
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+}
